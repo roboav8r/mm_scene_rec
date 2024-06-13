6a5b3193e6da1ca6f5b3746fd64c926efd59efe6
John Duncan <john.a.duncan@utexas.edu>
2024-06-12 21:39:07 -0500

    Add CPJKU's DCASE 2023 scene rec

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # mm_scene_rec
+
+# Future Work
+## Improvements
+- Fix checkpoint path in audio_scene_rec
+- Replace cpu() and .cuda() in audio_scene_rec with self.device as appropriate
--- scripts/audio_scene_rec.py
+++ scripts/audio_scene_rec.py
@@ -6,7 +6,10 @@ import torch
 import numpy as np
 import importlib
 import librosa
+import pytorch_lightning as pl
 import torchaudio.transforms as T
+import pathlib
+import time
 
 import rclpy
 from rclpy.node import Node
@@ -15,12 +18,84 @@ from ament_index_python.packages import get_package_share_directory
 from audio_common_msgs.msg import AudioDataStamped
 from std_msgs.msg import String
 
+from situated_hri_interfaces.msg import CategoricalDistribution
+
+from mm_scene_rec.cpjku_dcase23.models.cp_mobile_clean import get_model
+from mm_scene_rec.cpjku_dcase23.models.mel import AugmentMelSTFT
+
+# Helper functions
+def fuse_model(module):
+    # fuse layers
+    module.model.eval()  # only works in eval mode
+    module.model.cpu()
+    module.model.fuse_model()
+
+    # put original net back on cuda
+    module.model.cuda()
+
+def load_pretrained_from_id(module):
+    ckpt_path = os.path.join(get_package_share_directory('mm_scene_rec'),'config', "checkpoints")
+    assert os.path.exists(ckpt_path), f"No checkpoint path '{ckpt_path}' found."
+    ckpt_files = [file for file in pathlib.Path(os.path.expanduser(ckpt_path)).rglob('*.ckpt')]
+    assert len(ckpt_files) > 0, f"No checkpoint files found in path {ckpt_path}."
+    latest_ckpt = sorted(ckpt_files)[-1]
+    state_dict = torch.load(latest_ckpt)['state_dict']
+    # remove "model" prefix
+    state_dict = {k[len("model."):]: state_dict[k] for k in state_dict.keys()}
+    module.model.load_state_dict(state_dict)
+
+class PLModule(pl.LightningModule):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config  
+        # model to preprocess waveforms into log mel spectrograms
+        self.mel = AugmentMelSTFT(n_mels=config['n_mels'],
+                                  sr=config['resample_rate'],
+                                  win_length=config['window_size'],
+                                  hopsize=config['hop_size'],
+                                  n_fft=config['n_fft'],
+                                  freqm=config['freqm'],
+                                  timem=config['timem'],
+                                  fmin=config['fmin'],
+                                  fmax=None,
+                                  fmin_aug_range=config['fmin_aug_range'],
+                                  fmax_aug_range=config['fmax_aug_range']
+                                  )
+
+        # CP-Mobile
+        self.model = get_model(n_classes=config['n_classes'],
+                               in_channels=config['in_channels'],
+                               base_channels=config['base_channels'],
+                               channels_multiplier=config['channels_multiplier'],
+                               expansion_rate=config['expansion_rate']
+                               )
+
+    def mel_forward(self, x):
+        """
+        @param x: a batch of raw signals (waveform)
+        return: a batch of log mel spectrograms
+        """
+        old_shape = x.size()
+        x = x.reshape(-1, old_shape[2])  # for calculating log mel spectrograms we remove the channel dimension
+        x = self.mel(x)
+        x = x.reshape(old_shape[0], old_shape[1], x.shape[1], x.shape[2])  # batch x channels x mels x time-frames
+        return x
+
+    def forward(self, x):
+        """
+        :param x: batch of spectrograms
+        :return: final model predictions
+        """
+        x = self.model(x)
+        return x
+
 class SceneRecNode(Node):
 
     def __init__(self):
         super().__init__('scene_rec_node')
         self.subscription = self.create_subscription(AudioDataStamped, 'audio_data', self.audio_data_callback, 10)
-        self.audio_scene_publisher = self.create_publisher(String, 'audio_scene', 10)
+        self.scene_pub = self.create_publisher(String, 'audio_scene', 10)
+        self.scene_category_pub = self.create_publisher(CategoricalDistribution, 'audio_scene_category', 10)
         
         # Declare parameters with default values
         self.declare_parameter('n_channels', rclpy.Parameter.Type.INTEGER)
@@ -30,9 +105,6 @@ class SceneRecNode(Node):
         self.declare_parameter('scene_size', rclpy.Parameter.Type.INTEGER)
         self.declare_parameter('scene_index', rclpy.Parameter.Type.INTEGER_ARRAY)
         self.declare_parameter('scene_est_interval', rclpy.Parameter.Type.DOUBLE)
-        self.declare_parameter('model_path', rclpy.Parameter.Type.STRING)
-        self.declare_parameter('mean_path', rclpy.Parameter.Type.STRING)
-        self.declare_parameter('std_path', rclpy.Parameter.Type.STRING)
         self.declare_parameter('labels', rclpy.Parameter.Type.STRING_ARRAY)
 
         # Retrieve parameters
@@ -43,139 +115,127 @@ class SceneRecNode(Node):
         self.scene_size = self.get_parameter('scene_size').get_parameter_value().integer_value
         self.scene_idx = self.get_parameter('scene_index').get_parameter_value().integer_array_value
         self.scene_est_interval = self.get_parameter('scene_est_interval').get_parameter_value().double_value
-        self.model_path = os.path.join(get_package_share_directory('situated_interaction'),self.get_parameter('model_path').get_parameter_value().string_value)
-        self.mean_path = os.path.join(get_package_share_directory('situated_interaction'),self.get_parameter('mean_path').get_parameter_value().string_value)
-        self.std_path = os.path.join(get_package_share_directory('situated_interaction'),self.get_parameter('std_path').get_parameter_value().string_value)
         self.audio_scene_labels = self.get_parameter('labels').get_parameter_value().string_array_value
+
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
         # Audio data storage
         # TODO - get datatype from config file
-        # self.frame = torch.zeros([self.frame_size*self.n_channels],dtype=torch.float16)
         self.frame = torch.zeros([self.frame_size, self.n_channels],dtype=torch.float16)
         self.scene_audio = torch.zeros([self.scene_size, len(self.scene_idx)],dtype=torch.float16)
-        self.scene_audio = self.scene_audio.to('cuda')
+        self.scene_audio = self.scene_audio.to(self.device)
 
-        # Load scene rec model and dataset norm weights
+        # Module configuration
+        self.module_config = {}
+
+        self.declare_parameter('project_name', rclpy.Parameter.Type.STRING)
+        self.declare_parameter('wandb_id', rclpy.Parameter.Type.STRING)
+        self.project_name = self.get_parameter('project_name').get_parameter_value().string_value
+        self.wandb_id = self.get_parameter('wandb_id').get_parameter_value().string_value
+
+        self.declare_parameter('n_classes', rclpy.Parameter.Type.INTEGER)
+        self.declare_parameter('in_channels', rclpy.Parameter.Type.INTEGER)
+        self.declare_parameter('base_channels', rclpy.Parameter.Type.INTEGER)
+        self.declare_parameter('channels_multiplier', rclpy.Parameter.Type.DOUBLE)
+        self.declare_parameter('expansion_rate', rclpy.Parameter.Type.INTEGER)
+        self.module_config['n_classes'] = self.get_parameter('n_classes').get_parameter_value().integer_value
+        self.module_config['in_channels'] = self.get_parameter('in_channels').get_parameter_value().integer_value
+        self.module_config['base_channels'] = self.get_parameter('base_channels').get_parameter_value().integer_value
+        self.module_config['channels_multiplier'] = self.get_parameter('channels_multiplier').get_parameter_value().double_value
+        self.module_config['expansion_rate'] = self.get_parameter('expansion_rate').get_parameter_value().integer_value
+
+        self.declare_parameter('resample_rate', rclpy.Parameter.Type.INTEGER)
+        self.declare_parameter('window_size', rclpy.Parameter.Type.INTEGER)
+        self.declare_parameter('hop_size', rclpy.Parameter.Type.INTEGER)
+        self.declare_parameter('n_fft', rclpy.Parameter.Type.INTEGER)
+        self.declare_parameter('n_mels', rclpy.Parameter.Type.INTEGER)
+        self.declare_parameter('freqm', rclpy.Parameter.Type.INTEGER)
+        self.declare_parameter('timem', rclpy.Parameter.Type.INTEGER)
+        self.declare_parameter('fmin', rclpy.Parameter.Type.INTEGER)
+        self.declare_parameter('fmax', rclpy.Parameter.Type.INTEGER)
+        self.declare_parameter('fmin_aug_range', rclpy.Parameter.Type.INTEGER)
+        self.declare_parameter('fmax_aug_range', rclpy.Parameter.Type.INTEGER)    
+        self.module_config['resample_rate'] = self.get_parameter('resample_rate').get_parameter_value().integer_value
+        self.module_config['window_size'] = self.get_parameter('window_size').get_parameter_value().integer_value
+        self.module_config['hop_size'] = self.get_parameter('hop_size').get_parameter_value().integer_value
+        self.module_config['n_fft'] = self.get_parameter('n_fft').get_parameter_value().integer_value
+        self.module_config['n_mels'] = self.get_parameter('n_mels').get_parameter_value().integer_value
+        self.module_config['freqm'] = self.get_parameter('freqm').get_parameter_value().integer_value
+        self.module_config['timem'] = self.get_parameter('timem').get_parameter_value().integer_value
+        self.module_config['fmin'] = self.get_parameter('fmin').get_parameter_value().integer_value
+        self.module_config['fmax'] = self.get_parameter('fmax').get_parameter_value().integer_value
+        self.module_config['fmin_aug_range'] = self.get_parameter('fmin_aug_range').get_parameter_value().integer_value
+        self.module_config['fmax_aug_range'] = self.get_parameter('fmax_aug_range').get_parameter_value().integer_value
+
+        # Load scene rec model and resampler object
         self.init_model()
-        self.load_mean_std()
         self.resampler = T.Resample(self.sample_rate, self.downsample_rate, dtype=torch.float16)
-        self.resampler = self.resampler.to('cuda')
+        self.resampler = self.resampler.to(self.device)
 
     def init_model(self):
 
-        with open(self.model_path+'model_config.json',"r")as f:
-            model_config=json.load(f)
-       
-        module = importlib.import_module('situated_interaction.models.{}'.format(model_config['arch']))
-        Network = getattr(module, 'Network')
-        self.model=Network(model_config)
+        # create pytorch lightening module
+        self.pl_module = PLModule(self.module_config)
 
-        # Load pretrained weights
-        state_dict=torch.load(self.model_path+"/model_state_dict.pth")
+        # load model
+        load_pretrained_from_id(self.pl_module)
 
-        # if the weights are float16 cast the model
-        if [v for k,v in state_dict.items()][0].dtype==torch.float16:
-            self.model=self.model.half()
-        self.model.load_state_dict(state_dict)
+        # fuse layers
+        fuse_model(self.pl_module)
 
-        self.model.cuda()
-        self.model.eval()
-        self.model = self.model.to('cuda')
-    
-    def load_mean_std(self):
-        self.tr_mean = torch.load(self.mean_path)
-        self.tr_std = torch.load(self.std_path)
+        self.pl_module.mel.cuda()
 
-        self.tr_mean = self.tr_mean.to('cuda')
-        self.tr_std = self.tr_std.to('cuda')
-
-        self.tr_mean.half()
-        self.tr_std.half()        
-
-    def processor_d18_stereo_tensor(self, tensor):
-        n_fft = 2048  # 2048
-        sr = 22050  # 22050  # 44100  # 32000
-        n_mels = 256
-
-        hop_length = 512
-        fmax = None
-
-        self.spectrograms = []
-
-        for y in tensor:
-
-            y = y.to('cpu').numpy()
-
-            # compute stft
-            stft = librosa.stft(np.asfortranarray(y), n_fft=n_fft, hop_length=hop_length, win_length=None, window='hann', center=True,
-                                pad_mode='reflect')
-
-            # keep only amplitures
-            stft = np.abs(stft)
-
-            # spectrogram weighting
-            freqs = librosa.core.fft_frequencies(sr=sr, n_fft=n_fft)
-            stft = librosa.perceptual_weighting(stft ** 2, freqs, ref=1.0, amin=1e-10, top_db=80.0)
-
-            # apply mel filterbank
-            spectrogram = librosa.feature.melspectrogram(S=stft, sr=sr, n_mels=n_mels, fmax=fmax)
-
-            # keep spectrogram
-            self.spectrograms.append(np.asarray(spectrogram))
-
-        self.spectrograms = np.asarray(self.spectrograms, dtype=np.float32)
 
     def audio_data_callback(self, msg):
 
-        # self.get_logger().info('Got audio data with size %s' % (str(len(msg.audio.data))))
+        start_time = time.time()
 
         chunk = torch.from_numpy(np.frombuffer(msg.audio.data,dtype=np.float16)).view(-1,self.n_channels)
-
-        # self.get_logger().info('Got chunk with size %s' % (str(chunk.size())))
 
         # Roll the frame, and replace oldest contents with new chunk
         self.frame = torch.roll(self.frame, -chunk.size(0), 0)
         self.frame[-chunk.size(0):,:] = -chunk
 
-        # self.get_logger().info('Computed frame with size %s' % (str(self.frame.size())))
-
         torch.save(self.frame,'frame_data_recovered.pt')
 
+        # Reformat data as needed
         self.scene_audio = self.frame[:,self.scene_idx]
-        # self.get_logger().info('Computed scene audio with size %s' % (str(self.scene_audio.size())))
-        self.scene_audio = self.scene_audio.to('cuda')
+        self.scene_audio = self.scene_audio.to(self.device)
         torch.save(self.scene_audio,'scene_data_recovered.pt')
 
         resampled_sig = self.resampler(self.scene_audio.T)
-
         torch.save(resampled_sig,'scene_data_resampled.pt')
 
-        self.processor_d18_stereo_tensor(resampled_sig)
+        unsqueezed_sig = resampled_sig.unsqueeze(0)
+        unsqueezed_sig_float = unsqueezed_sig.float()
 
-        # classify
-        x = torch.from_numpy(self.spectrograms).to('cuda')
-        x = x.half()
+        # Run inference
+        mel_sig = self.pl_module.mel_forward(unsqueezed_sig_float)
+        torch.save(mel_sig,'scene_data_mel.pt')
 
-        x.unsqueeze_(0)
+        scene_est = self.pl_module.forward(mel_sig)
+        torch.save(scene_est,'scene_data_est.pt')
 
-        # print(x.shape)
-        x = (x - self.tr_mean) / self.tr_std
-        x = x.half()
+        scene_probs = scene_est.softmax(dim=-1).half().detach().cpu().numpy()
 
-        out=self.model(x)
+        # Output
+        msg = String()
+        msg.data = 'Classes: %s \n Probabilities: %s' % (self.audio_scene_labels, str(scene_probs))
+        self.scene_pub.publish(msg)
 
-        # Normalize logits
-        probs = torch.nn.functional.softmax(out, dim=1)
-        conf, class_idx = torch.max(probs, 1)
+        scene_category_msg = CategoricalDistribution()
+        scene_category_msg.categories = self.audio_scene_labels
+        scene_category_msg.probabilities = scene_probs[0].tolist()
+        self.scene_category_pub.publish(scene_category_msg)
 
-        scene_msg = String()
-        scene_msg.data = "Class: %s, %s%%; probs: %s" % (self.audio_scene_labels[class_idx], conf.item(), str(probs))
-        self.audio_scene_publisher.publish(scene_msg)
+        self.get_logger().debug("Inference time (s): %s" % (time.time() - start_time))
+
 
 def main(args=None):
     rclpy.init(args=args)
     audio_proc_node = SceneRecNode()
     rclpy.spin(audio_proc_node)
+
     audio_proc_node.destroy_node()
     rclpy.shutdown()
 
