d262730677625ac1997158fd73b5e5ad42259be5
John Duncan <john.a.duncan@utexas.edu>
2024-11-21 10:29:28 -0600

    Investigate whether or not order madders

--- scripts/multimodal_bayes_scene_est.py
+++ scripts/multimodal_bayes_scene_est.py
@@ -13,8 +13,6 @@ from std_srvs.srv import Empty
 
 from situated_hri_interfaces.msg import CategoricalDistribution
 
-
-
 def pmf_to_spec(pmf):
 
     spec = ''
@@ -49,15 +47,21 @@ class BayesSceneEstNode(Node):
         self.loop_time_sec = self.get_parameter('loop_time_sec').get_parameter_value().double_value
 
         # Initialize scene estimate
-        self.scene_symbol = gtsam.symbol('s',0)
-        self.scene_prob_est = gtsam.DiscreteDistribution([self.scene_symbol,len(self.scene_labels)],self.scene_probs)
+        self.audiofirst_scene_symbol = gtsam.symbol('s',0)
+        self.audiofirst_scene_prob_est = gtsam.DiscreteDistribution([self.audiofirst_scene_symbol,len(self.scene_labels)],self.scene_probs)
+        self.audiofirst_initialized = False
+
+        self.visionfirst_scene_symbol = gtsam.symbol('s',1)
+        self.visionfirst_scene_prob_est = gtsam.DiscreteDistribution([self.visionfirst_scene_symbol,len(self.scene_labels)],self.scene_probs)
+        self.visionfirst_initialized = False
 
         # Create callback groups
         self.timer_cb_group = MutuallyExclusiveCallbackGroup()
         self.sub_srv_cb_group = MutuallyExclusiveCallbackGroup()
 
         # Setup scene publisher
-        self.scene_category_pub = self.create_publisher(CategoricalDistribution, '~/fused_scene_category', 10)
+        self.scene_audiofirst_category_pub = self.create_publisher(CategoricalDistribution, '~/fused_scene_category_audiofirst', 10)
+        self.scene_visionfirst_category_pub = self.create_publisher(CategoricalDistribution, '~/fused_scene_category_visionfirst', 10)
 
         # Setup services
         self.reset_srv = self.create_service(Empty, '~/reset', self.reset_callback, callback_group=self.sub_srv_cb_group)
@@ -67,94 +71,135 @@ class BayesSceneEstNode(Node):
         self.update_timer = self.create_timer(self.loop_time_sec, self.update_callback, callback_group=self.timer_cb_group)
 
         # Get sensor parameters, form sensor param dictionary, setup subs
-        self.last_sensor_update_idx = None
-        self.next_sensor_update_idx = None
-        self.last_sensor_msg = dict()
-        self.sensor_params = dict()
-        self.declare_parameter('sensor_names',rclpy.Parameter.Type.STRING_ARRAY)
-        self.sensor_names = self.get_parameter('sensor_names').get_parameter_value().string_array_value
-        self.msg_is_new = [False]*len(self.sensor_names)
-
-        for sensor_idx, sensor_name in enumerate(self.sensor_names):
-            self.subscribers = []
-            self.sensor_params[sensor_name] = dict()
+        self.last_audio_msg = None
+        self.next_audiofirst_sensor = 'audio'
+        self.last_vision_msg = None
+        self.next_visionfirst_sensor = 'vision'
             
-            self.declare_parameter('%s.obs_labels' % sensor_name, rclpy.Parameter.Type.STRING_ARRAY)
-            self.declare_parameter('%s.topic' % sensor_name, rclpy.Parameter.Type.STRING)
-            self.declare_parameter('%s.sensor_model_coeffs' % sensor_name, rclpy.Parameter.Type.DOUBLE_ARRAY)
+        self.declare_parameter('audio_obs_labels', rclpy.Parameter.Type.STRING_ARRAY)
+        self.declare_parameter('audio_topic', rclpy.Parameter.Type.STRING)
+        self.declare_parameter('audio_sensor_model_coeffs', rclpy.Parameter.Type.DOUBLE_ARRAY)
+        self.audio_sensor_symbol = gtsam.symbol('o',0)
+        self.audio_obs_labels = self.get_parameter('audio_obs_labels').get_parameter_value().string_array_value
+        self.audio_sensor_model_coeffs = self.get_parameter('audio_sensor_model_coeffs').get_parameter_value().double_array_value
+        self.audio_sensor_model_array = np.array(self.audio_sensor_model_coeffs).reshape(-1,len(self.audio_obs_labels))
+        self.audio_sub = self.create_subscription(CategoricalDistribution,self.get_parameter('audio_topic').get_parameter_value().string_value, lambda msg: self.save_msg(msg, "audio"), 10, callback_group=self.sub_srv_cb_group)
 
-            self.sensor_params[sensor_name]['symbol'] = gtsam.symbol('o',sensor_idx)
-            self.sensor_params[sensor_name]['obs_labels'] = self.get_parameter('%s.obs_labels' % sensor_name).get_parameter_value().string_array_value
-            self.sensor_params[sensor_name]['sensor_model_coeffs'] = self.get_parameter('%s.sensor_model_coeffs' % sensor_name).get_parameter_value().double_array_value
-            self.sensor_params[sensor_name]['sensor_model_array'] = np.array(self.sensor_params[sensor_name]['sensor_model_coeffs']).reshape(-1,len(self.sensor_params[sensor_name]['obs_labels']))
-            self.sensor_params[sensor_name]['sensor_model'] = gtsam.DiscreteConditional([self.sensor_params[sensor_name]['symbol'],len(self.sensor_params[sensor_name]['obs_labels'])],[[self.scene_symbol,len(self.scene_labels)]],pmf_to_spec(self.sensor_params[sensor_name]['sensor_model_array']))
-
-            # self.get_logger().info(f'SENSOR PARAMS: {self.sensor_params[sensor_name]}')
-
-            self.subscribers.append(self.create_subscription(CategoricalDistribution,self.get_parameter('%s.topic' % sensor_name).get_parameter_value().string_value, eval("lambda msg: self.save_msg(msg, \"" + sensor_name + "\")",locals()), 10, callback_group=self.sub_srv_cb_group))
-
-    def publish_fused_scene(self):
-        scene_category_msg = CategoricalDistribution()
-        scene_category_msg.categories = self.scene_labels
-        scene_category_msg.probabilities = self.scene_prob_est.pmf()
-        self.scene_category_pub.publish(scene_category_msg)
+        self.declare_parameter('vision_obs_labels', rclpy.Parameter.Type.STRING_ARRAY)
+        self.declare_parameter('vision_topic', rclpy.Parameter.Type.STRING)
+        self.declare_parameter('vision_sensor_model_coeffs', rclpy.Parameter.Type.DOUBLE_ARRAY)
+        self.vision_sensor_symbol = gtsam.symbol('o',1)
+        self.vision_obs_labels = self.get_parameter('vision_obs_labels').get_parameter_value().string_array_value
+        self.vision_sensor_model_coeffs = self.get_parameter('vision_sensor_model_coeffs').get_parameter_value().double_array_value
+        self.vision_sensor_model_array = np.array(self.vision_sensor_model_coeffs).reshape(-1,len(self.vision_obs_labels))
+        self.vision_sub = self.create_subscription(CategoricalDistribution,self.get_parameter('vision_topic').get_parameter_value().string_value, lambda msg: self.save_msg(msg, "vision"), 10, callback_group=self.sub_srv_cb_group)
 
     def save_msg(self, msg, sensor_name):
 
-        sensor_idx = self.sensor_names.index(sensor_name)
+        if sensor_name=='audio':
+            self.last_audio_msg = msg
 
-        self.last_sensor_msg[sensor_name] = msg
-        self.msg_is_new[sensor_idx] = True
-
-        if self.next_sensor_update_idx is None:
-            self.next_sensor_update_idx = sensor_idx
+        if sensor_name=='vision':
+            self.last_vision_msg = msg
 
     def update_callback(self):
 
-        if self.next_sensor_update_idx is None:
-            return
-        
-        # If there is a new message available from the next sensor, update the scene estimate and indices
-        if self.msg_is_new[self.next_sensor_update_idx]:
+        # Update and publish audiofirst estimate
+        if (self.next_audiofirst_sensor == 'audio') & (self.last_audio_msg is not None):
 
-            # Update scene and indices
-            sensor_name = self.sensor_names[self.next_sensor_update_idx]
-            sensor_msg = self.last_sensor_msg[sensor_name]
-            
-            self.scene_update(sensor_msg, sensor_name)
+            obs = gtsam.DiscreteDistribution([self.audio_sensor_symbol,len(self.audio_obs_labels)],self.last_audio_msg.probabilities)
+            obs_factor = gtsam.DecisionTreeFactor(obs)
+            self.audio_sensor_model = gtsam.DiscreteConditional([self.audio_sensor_symbol,len(self.audio_obs_labels)],[[self.audiofirst_scene_symbol,len(self.scene_labels)]],pmf_to_spec(self.audio_sensor_model_array))
+            sensor_model_factor = gtsam.DecisionTreeFactor(self.audio_sensor_model)
+            likelihood = (obs_factor*sensor_model_factor).sum(1)
 
-            self.msg_is_new[self.next_sensor_update_idx] = False
-            self.last_sensor_update_idx = self.next_sensor_update_idx
-            self.next_sensor_update_idx = (self.next_sensor_update_idx + 1)%len(self.sensor_names)
+            self.audiofirst_scene_prob_est = gtsam.DiscreteDistribution(likelihood*self.audiofirst_scene_prob_est)
+
+            scene_category_msg = CategoricalDistribution()
+            scene_category_msg.categories = self.scene_labels
+            scene_category_msg.probabilities = self.audiofirst_scene_prob_est.pmf()
+            self.scene_audiofirst_category_pub.publish(scene_category_msg)
+
+            self.next_audiofirst_sensor = 'vision'
+
+        elif (self.next_audiofirst_sensor == 'vision') & (self.last_vision_msg is not None):
+
+            obs = gtsam.DiscreteDistribution([self.vision_sensor_symbol,len(self.vision_obs_labels)],self.last_vision_msg.probabilities)
+            obs_factor = gtsam.DecisionTreeFactor(obs)
+            self.vision_sensor_model = gtsam.DiscreteConditional([self.vision_sensor_symbol,len(self.vision_obs_labels)],[[self.audiofirst_scene_symbol,len(self.scene_labels)]],pmf_to_spec(self.vision_sensor_model_array))
+            sensor_model_factor = gtsam.DecisionTreeFactor(self.vision_sensor_model)
+            likelihood = (obs_factor*sensor_model_factor).sum(1)
+
+            self.audiofirst_scene_prob_est = gtsam.DiscreteDistribution(likelihood*self.audiofirst_scene_prob_est)
+
+            scene_category_msg = CategoricalDistribution()
+            scene_category_msg.categories = self.scene_labels
+            scene_category_msg.probabilities = self.audiofirst_scene_prob_est.pmf()
+            self.scene_audiofirst_category_pub.publish(scene_category_msg)
+
+            self.next_audiofirst_sensor = 'audio'
 
 
-    def scene_update(self,scene_msg, sensor_name):
+        # Update and publish visionfirst estimate
+        if (self.next_visionfirst_sensor == 'audio') & (self.last_audio_msg is not None):
 
-        # TODO - compute these and store them in sensor params beforehand to reduce unnecessary computation
-        obs = gtsam.DiscreteDistribution([self.sensor_params[sensor_name]['symbol'],len(self.sensor_params[sensor_name]['obs_labels'])],scene_msg.probabilities)
-        # likelihood = self.sensor_params[sensor_name]['sensor_model'].likelihood(obs.argmax())
+            obs = gtsam.DiscreteDistribution([self.audio_sensor_symbol,len(self.audio_obs_labels)],self.last_audio_msg.probabilities)
+            obs_factor = gtsam.DecisionTreeFactor(obs)
+            self.audio_sensor_model = gtsam.DiscreteConditional([self.audio_sensor_symbol,len(self.audio_obs_labels)],[[self.visionfirst_scene_symbol,len(self.scene_labels)]],pmf_to_spec(self.audio_sensor_model_array))
+            sensor_model_factor = gtsam.DecisionTreeFactor(self.audio_sensor_model)
+            likelihood = (obs_factor*sensor_model_factor).sum(1)
 
-        obs_factor = gtsam.DecisionTreeFactor(obs)
-        sensor_model_factor = gtsam.DecisionTreeFactor(self.sensor_params[sensor_name]['sensor_model'])
-        likelihood = (obs_factor*sensor_model_factor).sum(1)
+            self.visionfirst_scene_prob_est = gtsam.DiscreteDistribution(likelihood*self.visionfirst_scene_prob_est)
 
-        self.scene_prob_est = gtsam.DiscreteDistribution(likelihood*self.scene_prob_est)
+            scene_category_msg = CategoricalDistribution()
+            scene_category_msg.categories = self.scene_labels
+            scene_category_msg.probabilities = self.visionfirst_scene_prob_est.pmf()
+            self.scene_visionfirst_category_pub.publish(scene_category_msg)
 
-        self.publish_fused_scene()
+            self.next_visionfirst_sensor = 'vision'
+
+        elif (self.next_visionfirst_sensor == 'vision') & (self.last_vision_msg is not None):
+
+            obs = gtsam.DiscreteDistribution([self.vision_sensor_symbol,len(self.vision_obs_labels)],self.last_vision_msg.probabilities)
+            obs_factor = gtsam.DecisionTreeFactor(obs)
+            self.vision_sensor_model = gtsam.DiscreteConditional([self.vision_sensor_symbol,len(self.vision_obs_labels)],[[self.visionfirst_scene_symbol,len(self.scene_labels)]],pmf_to_spec(self.vision_sensor_model_array))
+            sensor_model_factor = gtsam.DecisionTreeFactor(self.vision_sensor_model)
+            likelihood = (obs_factor*sensor_model_factor).sum(1)
+
+            self.visionfirst_scene_prob_est = gtsam.DiscreteDistribution(likelihood*self.visionfirst_scene_prob_est)
+
+            scene_category_msg = CategoricalDistribution()
+            scene_category_msg.categories = self.scene_labels
+            scene_category_msg.probabilities = self.visionfirst_scene_prob_est.pmf()
+            self.scene_visionfirst_category_pub.publish(scene_category_msg)
+
+            self.next_visionfirst_sensor = 'audio'
+
 
     def reset_callback(self, request, response):
         self.get_logger().info('Resetting...')
-        self.scene_prob_est = gtsam.DiscreteDistribution([self.scene_symbol,len(self.scene_labels)],self.scene_probs)
+
+        self.audiofirst_scene_prob_est = gtsam.DiscreteDistribution([self.audiofirst_scene_symbol,len(self.scene_labels)],self.scene_probs)
+        self.audiofirst_initialized = False
+        self.last_audio_msg = None
+        self.next_audiofirst_sensor = 'audio'
+
+        self.visionfirst_scene_prob_est = gtsam.DiscreteDistribution([self.visionfirst_scene_symbol,len(self.scene_labels)],self.scene_probs)
+        self.visionfirst_initialized = False
+        self.last_vision_msg = None
+        self.next_visionfirst_sensor = 'vision'
+
         return response
 
     def reconf_callback(self, request, response):
         self.get_logger().info('Reconfiguring...')
 
-        for sensor_idx, sensor_name in enumerate(self.sensor_names):
-            self.sensor_params[sensor_name]['sensor_model_coeffs'] = self.get_parameter('%s.sensor_model_coeffs' % sensor_name).get_parameter_value().double_array_value
-            self.sensor_params[sensor_name]['sensor_model_array'] = np.array(self.sensor_params[sensor_name]['sensor_model_coeffs']).reshape(-1,len(self.sensor_params[sensor_name]['obs_labels']))
-            self.sensor_params[sensor_name]['sensor_model'] = gtsam.DiscreteConditional([self.sensor_params[sensor_name]['symbol'],len(self.sensor_params[sensor_name]['obs_labels'])],[[self.scene_symbol,len(self.scene_labels)]],pmf_to_spec(self.sensor_params[sensor_name]['sensor_model_array']))
+        self.audio_sensor_model_coeffs = self.get_parameter('audio_sensor_model_coeffs').get_parameter_value().double_array_value
+        self.audio_sensor_model_array = np.array(self.audio_sensor_model_coeffs).reshape(-1,len(self.audio_obs_labels))
 
+        self.vision_sensor_model_coeffs = self.get_parameter('vision_sensor_model_coeffs').get_parameter_value().double_array_value
+        self.vision_sensor_model_array = np.array(self.vision_sensor_model_coeffs).reshape(-1,len(self.vision_obs_labels))
+        
         return response
 
 
